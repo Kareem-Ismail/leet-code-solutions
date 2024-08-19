31feeed41f3b46e341c9cb08a002c477e840e718
kareemismail <kareem.ext@element.in>
2024-08-19 23:34:19 +0300

    Keeko :- SlidingWindow -> findMaxAverage

--- src/main/java/ArraysAndStrings/Keeko/SlidingWindow.java
+++ src/main/java/ArraysAndStrings/Keeko/SlidingWindow.java
@@ -2,4 +2,22 @@ package ArraysAndStrings.Keeko;
 
 public class SlidingWindow {
 
+    public double findMaxAverage(int[] nums, int k) {
+        double curr = 0;
+
+        for (int i = 0; i < k && i < nums.length; i++)
+            curr += nums[i];
+
+        double maxCurr = curr;
+
+        for (int left = 0, right = k; right < nums.length; left++, right++) {
+            curr -= nums[left];
+            curr += nums[right];
+            if(curr > maxCurr)
+                maxCurr = curr;
+        }
+
+        return maxCurr / k;
+    }
+
 }
